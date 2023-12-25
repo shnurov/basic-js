4b027807356379fe7e34519ffd44621315a9ee94
Pavel Shnurov <pavel.shnurov@gmail.com>
2023-12-25 12:27:32 +0400

    feat: add 1 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,9 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || Number(sampleActivity) <= 0 || Number(sampleActivity) >= MODERN_ACTIVITY || isNaN(sampleActivity)) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) * (HALF_LIFE_PERIOD / 0.693));
 }
 
 module.exports = {
