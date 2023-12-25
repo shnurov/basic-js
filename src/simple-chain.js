05e708ca77ba22a031320ae7ab7bf5073ea8ed1c
Pavel Shnurov <pavel.shnurov@gmail.com>
2023-12-25 17:21:15 +0400

    feat: add 16 task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+
+  arr: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position == 'number' && position >= 1 && position <= this.arr.length - 1) {
+      this.arr.splice(position -1, 1)
+      return this;
+    } else {
+      this.arr = [];
+      throw new Error("You can\'t remove incorrect link!"); 
+    }
+
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.arr.join('~~');
+    this.arr = [];
+    return res;
   }
 };
 
